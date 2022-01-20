415f3a74df8e1a838efb147d643d8e20966073b9
Dobrinya <dobrij13@gamil.com>
2022-01-20 12:41:14 +0200

    Start

--- src/main/Encryption.java
+++ src/main/Encryption.java
@@ -1,4 +1,10 @@
 package main;
 
 public class Encryption {
+    static String russianAlphabetToLowerCase = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя";
+    static String russianAlphabetToUpperCase = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ";
+    static String englishAlphabetToLowerCase = "abcdefghijklmnopqrstuvwxyz";
+    static String englishAlphabetToUpperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    static String numbers = "0123456789";
+    static String symbols = ".,”:-!? ";
 }
--- src/main/Program.java
+++ src/main/Program.java
@@ -2,6 +2,6 @@ package main;
 
 public class Program {
     public static void main(String[] args) {
-
+        System.out.println(Encryption.russianAlphabetToLowerCase);
     }
 }
